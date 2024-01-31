6aae7a5620a7e2b964281f0147c8e91ef0f22b6c
ks233 <2681780098@qq.com>
2024-01-31 19:53:22 +0800

    Update ants.py
    q11

--- week5/ants/ants.py
+++ week5/ants/ants.py
@@ -53,6 +53,7 @@ class Insect:
 
     damage = 0
     # ADD CLASS ATTRIBUTES HERE
+    is_watersafe = False
 
     def __init__(self, armor, place=None):
         """Create an Insect with an ARMOR amount and a starting PLACE."""
@@ -448,6 +449,9 @@ class Water(Place):
         its armor to 0."""
         # BEGIN Problem 11
         "*** YOUR CODE HERE ***"
+        Place.add_insect(self, insect)
+        if not insect.is_watersafe:
+            insect.reduce_armor(insect.armor)
         # END Problem 11
 
 
@@ -508,6 +512,7 @@ class Bee(Insect):
     name = "Bee"
     damage = 1
     # OVERRIDE CLASS ATTRIBUTES HERE
+    is_watersafe = True
 
     def sting(self, ant):
         """Attack an ANT, reducing its armor by 1."""
